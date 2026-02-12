90bdc01ba2143bfb5b5426c594f8248335739071
Samujal <samujalphukan@yahoo.com>
2026-02-12 19:53:26 +0530

    www

---
+++ login/src/services/token_service.rs
@@ -0,0 +1,11 @@
+use jsonwebtoken::{encode, Header, EncodingKey};
+use serde::{Serialize, Deserialize};
+use chrono::{Utc, Duration};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    pub sub: String,
+    pub exp: usize,
+}
+
+pub
