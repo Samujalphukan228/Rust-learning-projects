621d4804bf964ba68ddc8d6f7ab59885d83d60b4
Samujal <samujalphukan@yahoo.com>
2026-02-12 18:34:09 +0530

    22

--- login/src/errors.rs
+++ login/src/errors.rs
@@ -1,8 +1,7 @@
 use axum::{
     response::{IntoResponse, Response},
-    http:: StatusCode,
+    http::StatusCode,
 };
-
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -25,6 +24,20 @@ pub enum AppError {
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        
+        let status = match self {
+            AppError::UserExists => StatusCode::CONFLICT,
+            AppError::InvalidCredentials => StatusCode::UNAUTHORIZED,
+            AppError::Unauthorized => StatusCode::UNAUTHORIZED,
+            AppError::DatabaseError => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
+        (status, self.to_string()).into_response()
+    }
+}
+
+impl From<mongodb::error::Error> for AppError {
+    fn from(_: mongodb::error::Error) -> Self {
+        AppError::DatabaseError
     }
 }
