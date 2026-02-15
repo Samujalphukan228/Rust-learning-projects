41a287d699633b3bb006968bb86c4497824fe8c4
Samujal <samujalphukan@yahoo.com>
2026-02-15 19:02:09 +0530

    22

--- elasticsearch/server.js
+++ elasticsearch/server.js
@@ -455,6 +455,7 @@ app.get("/search/phrase", async (req, res) => {
 });
 
 // 4️⃣ WILDCARD SEARCH (pattern matching)
+
 app.get("/search/wildcard", async (req, res) => {
   try {
     const { q } = req.query;
